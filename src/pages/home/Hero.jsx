95d233cb49c0ec9d2f7080a321489167448002fd
Sikander Afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2023-07-03 20:23:58 +0500

    sad#

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,4 +1,49 @@
+import { useRef, useState, useEffect } from "react";
+
 const Hero = () => {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  let interval = useRef();
+  const startTimer = () => {
+    let end = new Date("7/15/2023 3:00 PM");
+    interval = setInterval(() => {
+      let _second = 1000;
+      let _minute = _second * 60;
+      let _hour = _minute * 60;
+      let _day = _hour * 24;
+      let now = new Date();
+      let nowUTC = new Date(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate(),
+        now.getUTCHours(),
+        now.getUTCMinutes(),
+        now.getUTCSeconds()
+      );
+      let distance = end - nowUTC;
+      var days = Math.floor(distance / _day);
+      var hours = Math.floor((distance % _day) / _hour);
+      var minutes = Math.floor((distance % _hour) / _minute);
+      var seconds = Math.floor((distance % _minute) / _second);
+      if (distance < 0) {
+        clearInterval(interval.current);
+        return;
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
   return (
     <section className="wrapper     xl:h-screen py-[140px] xl:py-0 xl:min-h-[850px]">
       <div className="contain xl:flex-row flex-col justify-between h-full items-center xl:gap-10">
@@ -32,19 +77,25 @@ const Hero = () => {
           </div>
           <div className="flex sm:scale-100 scale-[0.6] absolute right-0 md:right-[20%] bottom-[17%] gap-5 z-20 justify-center items-center w-full max-w-[340px] rounded-[20px] bg-hero backdrop-blur-[50px] shadow-hero p-5">
             <p className="text-white text-center font-comic text-lg font-normal">
-              <span className="font-sans text-[40px] leading-[1]">17</span>{" "}
+              <span className="font-sans text-[40px] leading-[1]">
+                {timerHours}
+              </span>{" "}
               <br />
               Hours:{" "}
             </p>
             <p className="font-normal text-[28px] text-white">:</p>
             <p className="text-white text-center font-comic text-lg font-normal">
-              <span className="font-sans text-[40px] leading-[1]">17</span>{" "}
+              <span className="font-sans text-[40px] leading-[1]">
+                {timerMinutes}
+              </span>{" "}
               <br />
               Minutes:{" "}
             </p>
             <p className="font-normal text-[28px] text-white">:</p>
             <p className="text-white text-center font-comic text-lg font-normal">
-              <span className="font-sans text-[40px] leading-[1]">17</span>{" "}
+              <span className="font-sans text-[40px] leading-[1]">
+                {timerSeconds}
+              </span>{" "}
               <br />
               Seconds:{" "}
             </p>
