e7d88d8b245fa83b00ee32209136dc0176341d3d
Sikander Afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2023-07-05 01:05:11 +0500

    asd

--- src/pages/home/Brands.jsx
+++ src/pages/home/Brands.jsx
@@ -3,8 +3,12 @@ const Brands = () => {
     <section className="wrapper relative -mt-[80px] sm:-mt-[130px] xl:mt-4 2xl:mt-[110px]">
       <div className="contain justify-start items-center flex-col gap-10">
         <div className="grid w-full grid-cols-2 lg:grid-cols-[auto__auto__auto__auto] place-items-center gap-x-10 sm:gap-[100px] xl:gap-x-[160px] gap-y-[40px]">
-          <img src="/pink.png" className="object-contain h-[60px]" alt="" />
-          <img src="/dex.png" className="object-contain h-[60px]" alt="" />
+          <a href="https://www.pinksale.finance/" target="blank">
+            <img src="/pink.png" className="object-contain h-[60px]" alt="" />
+          </a>
+          <a href="https://www.dexview.com/" target="blank">
+            <img src="/dex.png" className="object-contain h-[60px]" alt="" />
+          </a>
           <img src="/uni.png" className="object-contain h-[60px]" alt="" />
           <img src="/solid.png" className="object-contain h-[60px]" alt="" />
         </div>
